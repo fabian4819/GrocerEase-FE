9c33b8c6ddffe8b9935be4a27f1b61dff218b879
wulaneee <wulantiarahayu0903@mail.ugm.ac.id>
2024-11-24 17:06:26 +0700

    feat: menambahkan halaman login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
+import LoginButton from '../components/loginButton'
 
 const Login = () => {
+  console.log("Login component loaded");
   return (
-    <div>Ini Page Login</div>
-  )
-}
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Masuk ke Dashboard</h2>
+        <form>
+          <div className="input-box">
+            <label>Email</label>
+            <input type="email" placeholder="email@mail.com" required />
+          </div>
+          <div className="input-box">
+            <label>Password</label>
+            <input type="password" placeholder="password" required />
+          </div>
+          <button type="submit" className="submit-btn">
+            Masuk <span>➡️</span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
